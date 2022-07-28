e06159993f270c599af815e7059ac214b9a9b305
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-07-28 06:02:15 +0530

    [Shivaraj] Added Updating Entry in Address book

--- AddressBook/AddressBookRestsharp/Program.cs
+++ AddressBook/AddressBookRestsharp/Program.cs
@@ -14,3 +14,22 @@ namespace AddressBookRestSharp
         }
     }
 }
+
+/*
+ UC-24
+Ability to Update Entry in Address Book JSONServer and sync with Address Book Application Memory 
+- Use RESTSharp for REST Api Calls from MSTest Test Code
+
+Standard Output: 
+{
+  "firstname": "Yash",
+  "lastname": "V",
+  "phoneNo": "7858070934",
+  "address": "FC Real Madrid",
+  "city": "Madrid",
+  "state": "Spain",
+  "zip": "535678",
+  "email": "yash7@gmail.com",
+  "id": 7
+}
+ */
