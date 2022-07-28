7ac17a80432800b3e5e5c500f69f9a7799e3f62a
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-07-28 05:43:45 +0530

    [Shivaraj] Added test case for adding multiple entries

--- AddressBook/AddressBookRestsharp/Program.cs
+++ AddressBook/AddressBookRestsharp/Program.cs
@@ -14,3 +14,44 @@ namespace AddressBookRestSharp
         }
     }
 }
+
+/*
+ UC-23
+Ability to Add Multiple Entries to Address Book JSONServer and sync with Address Book Application Memory 
+- Use RESTSharp for REST Api Calls from MSTest Test Code
+
+Standard Output: 
+{
+  "firstname": "Arun",
+  "lastname": "R",
+  "phoneNumber": "9577456345",
+  "address": "Feroz Shah Kotla",
+  "city": "New Delhi",
+  "state": "New Delhi",
+  "zip": "547677",
+  "email": "vs@gmail.com",
+  "id": 5
+}
+{
+  "firstname": "Ankitha",
+  "lastname": "H",
+  "phoneNumber": "9756723456",
+  "address": "Chinnaswamy",
+  "city": "Bangalore",
+  "state": "Karnataka",
+  "zip": "435627",
+  "email": "yc@gmail.com",
+  "id": 6
+}
+{
+  "firstname": "Yash",
+  "lastname": "V",
+  "phoneNumber": "9954564345",
+  "address": "Mohali",
+  "city": "Mohali",
+  "state": "Punjab",
+  "zip": "113425",
+  "email": "klr@gmail.com",
+  "id": 7
+}
+ */
